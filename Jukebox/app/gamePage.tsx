815c18df5b76c856ff3c9687ae9d343586f04605
Caleb <calebzeringue25@gmail.com>
2025-03-12 21:09:02 -0500

    I created a fully functional navigation bar that sends you to each of the corresponding pages!! If you get any errors just make sure to download the stuff you need to make it work!

---
+++ Jukebox/app/gamePage.tsx
@@ -0,0 +1,13 @@
+// app/search.tsx
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+export default function GamePage() {
+  return (
+    <View>
+      <Text>Game Page</Text>
+      
+    </View>
+  );
+}
