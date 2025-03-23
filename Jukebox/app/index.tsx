61f84b52a825911e4b4d2585f8f1becbe90e0725
NukeApprentice <darrionpaul2@gmail.com>
2025-03-22 23:12:44 -0500

    Made base Sign in Screen

--- Jukebox/app/index.tsx
+++ Jukebox/app/index.tsx
@@ -1,18 +1,11 @@
 import React from "react";
-import { Text, View, ScrollView, StyleSheet, SafeAreaView } from "react-native";
-export default function Index() {
+import { View, Text } from "react-native";
+import SignIn from "@/components/SignIn";
 
+export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "black",
-      }}
-    >
-      <Text>Music Hello</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "black" }}>
+      <SignIn />
     </View>
   );
 }
-
