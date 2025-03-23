f0bd36104b5c44e2652bdd434bd09b0a483c1b40
boug1e <mbourgeois20783@gmail.com>
2025-03-22 20:57:13 -0500

    some files were missing React import for some reason.

--- Jukebox/app/_layout.tsx
+++ Jukebox/app/_layout.tsx
@@ -1,11 +1,12 @@
+import React from 'react';
 import { Stack } from "expo-router";
 import { SafeAreaView } from 'react-native-safe-area-context';
-import NavBar from './Components/NavBar';
+import NavBar from '@/components/NavBar';
 
 export default function RootLayout() {
   return 
   <SafeAreaView style={{ flex: 1 }}>
-  <Stack />;
-  <NavBar />
+    <Stack />;
+    <NavBar />
   </SafeAreaView>
 }
